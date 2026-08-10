a20c03cea1c9dfb0c857ebf6a2c9d100cdec1f45
Vedant Tiwari <vedantverse@gmail.com>
2026-08-10 16:20:24 +0530

    feat: added 6 high-intent performance marketing blogs aligned with client case studies for SEO/AEO/GEO optimization

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next';
 import { CLIENTS_DATA } from '../data/clientsData';
+import { getAllBlogs } from '../data/blogs';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://vedantverse.in';
@@ -7,6 +8,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const clientPages: MetadataRoute.Sitemap = CLIENTS_DATA.map((client) => ({
     url: `${baseUrl}/clients/${client.slug}`,
     lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }));
+
+  const allBlogs = getAllBlogs();
+  const blogPages: MetadataRoute.Sitemap = allBlogs.map((blog) => ({
+    url: `${baseUrl}/blog/${blog.slug}`,
+    lastModified: new Date(blog.publishDate || Date.now()),
     changeFrequency: 'weekly',
     priority: 0.8,
   }));
@@ -22,7 +31,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
-      priority: 0.8,
+      priority: 0.9,
     },
     {
       url: `${baseUrl}/privacy`,
@@ -44,5 +53,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ];
 
-  return [...staticPages, ...clientPages];
+  return [...staticPages, ...clientPages, ...blogPages];
 }
